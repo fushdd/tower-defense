a2ce06dfa2cf8b2d5984b013d519456601418e73
fushdd <daniil.vladimirov.07@gmail.com>
2026-06-18 16:59:43 -0400

    health bar

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,14 +1,26 @@
+using TMPro;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public float health = 5f;
+    public float curHealth = 5f;
+    public float MaxHealth = 5f;
+
+    private Transform healthFillVisual;
+    public TMP_Text healthTextVisual;
 
     public void TakeDamage(float damage)
     {
-        health -= damage;
+        curHealth -= damage;
 
-        if (health <= 0)
+        // scale and reposition health fill ui
+        healthFillVisual.localScale = new Vector3(curHealth / MaxHealth, 1, 1);
+        healthFillVisual.localPosition = new Vector3((curHealth / MaxHealth - 1) / 2, 0, 0);
+
+        // change text values
+        healthTextVisual.text = $"{curHealth}/{MaxHealth}";
+
+        if (curHealth <= 0)
         {
             Die();
         }
@@ -17,5 +29,12 @@ public class EnemyHealth : MonoBehaviour
     void Die()
     {
         Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        //healthFillVisual = transform.Find("HealthFill");
+        healthFillVisual = transform.GetChild(0).Find("HealthFill");
+        healthTextVisual.text = $"{curHealth}/{MaxHealth}";
     }
 }
